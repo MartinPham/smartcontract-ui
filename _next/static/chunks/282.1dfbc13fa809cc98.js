60040f4f682fd14942d9f8e4fb70cbc50becdf08
MartinPham <MartinPham@users.noreply.github.com>
2022-01-03 21:17:35 +0000

    Manual build 1403250

---
+++ _next/static/chunks/282.1dfbc13fa809cc98.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[282],{52361:function(){},94616:function(){},42602:function(){}}]);
--- config/chains.ts
+++
@@ -1,4 +0,0 @@
-import { Chain, RpcUrlMap } from 'types/Chain'
-export const chains: Chain[] = require('config/chains.json')
-export const rpcs: RpcUrlMap = require('config/rpcs.json')
-export const chainIds: number[] = require('config/chainIds.json')
